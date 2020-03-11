fbb53d0249d1735d00ae7681d50d311a9cb5ebb9
endorphin82 <endorphin82@gmail.com>
2020-03-11 21:14:11 +0200

    Updates

---
+++ precache-manifest.ac7bb944a684cc8f977fa8dd8436e4c0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4006bcd19a8d43786c4ed1d341632334",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e0e9f101056365622d28",
+    "url": "./static/css/main.350bf5a3.chunk.css"
+  },
+  {
+    "revision": "7da37e9407031f41e92f",
+    "url": "./static/js/2.5c60f4ae.chunk.js"
+  },
+  {
+    "revision": "94b29ac6d0ec426932e82da99c88b81c",
+    "url": "./static/js/2.5c60f4ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0e9f101056365622d28",
+    "url": "./static/js/main.b9e929b4.chunk.js"
+  },
+  {
+    "revision": "b34f9a2aa82a1adf794f",
+    "url": "./static/js/runtime-main.a22dffc0.js"
+  },
+  {
+    "revision": "7fe95501b6513e4406073d66d8cca1a6",
+    "url": "./static/media/render.7fe95501.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.c1814bfeaa82526d47defc198e3c56ca.js"
+  "./precache-manifest.ac7bb944a684cc8f977fa8dd8436e4c0.js"
 );
 
 self.addEventListener('message', (event) => {
